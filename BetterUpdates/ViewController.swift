7ffaacbba51fa299952daad49bba507628b7f3ef
Philippe Weidmann <philweidmann@me.com>
2017-02-15 09:26:45 +0100

    Initial Commit

---
+++ BetterUpdates/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BetterUpdates
+//
+//  Created by Philippe Weidmann on 15.02.17.
+//  Copyright © 2017 Philippe Weidmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
